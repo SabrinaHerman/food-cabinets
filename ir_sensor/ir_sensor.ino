d42da8088292053a248ce7a09bd28da4b6aa4d39
Caitlin Stanton <cs968@cornell.edu>
2021-05-05 14:35:02 -0400

    multiple IR sensors

--- ir_sensor/ir_sensor.ino
+++ ir_sensor/ir_sensor.ino
@@ -5,6 +5,8 @@ int count = 0;
 void setup(void) {
   pinMode(13,OUTPUT);
   Serial.begin(9600);
+  
+  float fullDiagonal = 32 * 2.54; // distance from front bottom left corner to the back top right corner of the shelf (conversion from in to cm)
 }
 
 void loop(void) {
@@ -12,9 +14,18 @@ void loop(void) {
   if (count % 2 == 0) {
     digitalWrite(13, HIGH);
     Serial.println("on");
-    int reading = analogRead(A1);
+    int tl = analogRead(A1);
+    int bl = analogRead(A2);
+    int tr = analogRead(A3);
+    int br = analogRead(A4);
 
-    int calculated = (6762/(reading-9))-4;
+    int topLeft = (6762/(tl-9))-4;
+    int botLeft = (6762/bl-9))-4;
+    int topRight = (6762/(tr-9))-4;
+    int botRight = (6762/(br-9))-4;
+
+    int tl_br = fullDiagonal - topLeft - bottomRight;
+    int tr_bl = fullDiagonal - topRight - bottomLeft
   
     Serial.println(calculated);
   
